e4f834e278c486b0373394ddbedfa374011d4da3
z.j <jwzheng0410@hotmail.com>
2022-08-10 15:56:03 +0800

    read task from excel and auto schema success

--- bq_migrate_pipeline.py
+++ bq_migrate_pipeline.py
@@ -1,86 +1,82 @@
-import argparse
 import logging
+import os
+
+import pandas as pd
+import numpy as np
 
 import apache_beam as beam
+from apache_beam.io.gcp.internal.clients.bigquery import TableSchema, TableFieldSchema
 
 
-def run(argv=None):
+def read_from_task(task_file = 'task.xlsx'):
+    task_file = os.path.abspath(task_file)
+    xl_file = pd.ExcelFile(task_file)
+    task = None
+    try:
+        task_list = list(xl_file.parse().replace({np.nan:None}).T.to_dict().values())
+    except Exception as e:
+        raise e
+    for task in task_list:
+        if not task['target_table']:
+            task['target_table'] = task['source_table']
+    return task_list
+
+def get_table_schema(table):
+    from google.cloud import bigquery
+    client =  bigquery.Client()
+    dataset_ref = client.dataset(table['source_dataset'], project=table['source_project'])
+    table_ref = dataset_ref.table(table['source_table'])
+    table = client.get_table(table_ref)  
+    schema_field_list = table.schema
+
+    # Convert SchemaField type to TableSchema, which beam.io.WriteToBigQuery used to start jobs.
+    table_schema = TableSchema()
+    table_schema.fields = [schemafield_to_tableschema(schema_field) for schema_field in schema_field_list]
+    return table_schema
+
+def schemafield_to_tableschema(schema_field):
+    """
+        beam.io.WriteToBigQuery takes TableSchema object as parameter, while bigquery client returns SchemaField object, so need to convert here.
+    """
+    table_field_schema = TableFieldSchema()
+    table_field_schema.name = schema_field.name
+    table_field_schema.type = schema_field.field_type
+    table_field_schema.mode = schema_field.mode
+    table_field_schema.policyTags = schema_field.policy_tags
+    table_field_schema.description = schema_field.description
+    table_field_schema.fields = list(schema_field.fields)
+    return table_field_schema
+
+
+def run():
+    task_list = read_from_task()
+    for task in task_list:
+        migrate_table(task)
+
+
+def migrate_table(task):
     """Run the workflow."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--input',
-        dest='input',
-        required=True,
-        help=(
-            'Input(source) BigQuery table specified as: '
-            'PROJECT:DATASET.TABLE or DATASET.TABLE.'))
+    table_schema = get_table_schema(task)
 
-    parser.add_argument(
-        '--output',
-        dest='output',
-        required=True,
-        help=(
-            'Output(destination) BigQuery table for results specified as: '
-            'PROJECT:DATASET.TABLE or DATASET.TABLE.'))
-    known_args, pipeline_args = parser.parse_known_args(argv)
+    input = task['source_project'] + ':' + task['source_dataset'] + '.' + task['source_table']
+    output = task['target_project'] + ':' + task['target_dataset']+ '.' + task['target_table']
+
+    pipeline_args = []
     pipeline_args.extend([
         '--runner=DataflowRunner',
         '--project=zheng-356906',
-
         #'--region=asia-east2',
-        
-        #'--staging_location=gs://YOUR_BUCKET_NAME/AND_STAGING_DIRECTORY',
-        #'--temp_location=gs://YOUR_BUCKET_NAME/AND_TEMP_DIRECTORY',
-        #'--job_name=your-wordcount-job',
+        '--staging_location=gs://jiawei_bucket/staging',
+        '--temp_location=gs://jiawei_bucket/tmp',
     ])
 
     with beam.Pipeline(argv=pipeline_args) as p:
 
         table_records = p | 'ReadTable' >> beam.io.ReadFromBigQuery(
-            table=known_args.input)
-
-        from apache_beam.io.gcp.internal.clients import bigquery
-        table_schema = bigquery.TableSchema()
-
-        # Fields that use standard types.
-        schema1 = bigquery.TableFieldSchema()
-        schema1.name = 'date'
-        schema1.type = 'DATE'
-        schema1.mode = 'nullable'
-        table_schema.fields.append(schema1)
-
-        schema2 = bigquery.TableFieldSchema()
-        schema2.name = 'country_name'
-        schema2.type = 'string'
-        schema2.mode = 'nullable'
-        table_schema.fields.append(schema2)
-
-        schema3 = bigquery.TableFieldSchema()
-        schema3.name = 'new_confirmed'
-        schema3.type = 'integer'
-        schema3.mode = 'nullable'
-        table_schema.fields.append(schema3)
-
-        schema4 = bigquery.TableFieldSchema()
-        schema4.name = 'new_deceased'
-        schema4.type = 'integer'
-        schema4.mode = 'nullable'
-        table_schema.fields.append(schema4)
-
-        schema5 = bigquery.TableFieldSchema()
-        schema5.name = 'cumulative_persons_fully_vaccinated'
-        schema5.type = 'integer'
-        schema5.mode = 'nullable'
-        table_schema.fields.append(schema5)
-
-        schema6 = bigquery.TableFieldSchema()
-        schema6.name = 'population'
-        schema6.type = 'integer'
-        schema6.mode = 'nullable'
-        table_schema.fields.append(schema6)
+            table=input)
 
         table_records | 'WriteTable' >> beam.io.WriteToBigQuery(
-            known_args.output,
+            output,
             schema=table_schema,
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
             write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
@@ -88,5 +84,6 @@ def run(argv=None):
 
 
 if __name__ == '__main__':
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\\zj\\coding\\GCP\\zheng-356906-9b7969f2ccbb.json"
     logging.getLogger().setLevel(logging.INFO)
     run()
