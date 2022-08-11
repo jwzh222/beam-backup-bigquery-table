7524acbc28ddf0a9217187d641ae47657a43d797
z.j <jwzheng0410@hotmail.com>
2022-08-11 17:24:12 +0800

    threading on DirectRunner ok, Dataflow not ok

--- bq_migrate_pipeline.py
+++ bq_migrate_pipeline.py
@@ -1,3 +1,5 @@
+import argparse
+from concurrent.futures import ThreadPoolExecutor
 import logging
 import os
 
@@ -7,13 +9,17 @@ import numpy as np
 import apache_beam as beam
 from apache_beam.io.gcp.internal.clients.bigquery import TableSchema, TableFieldSchema
 
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
 
-def read_from_task(task_file = 'task.xlsx'):
+
+def read_from_task_file(task_file='task.xls'):
     task_file = os.path.abspath(task_file)
     xl_file = pd.ExcelFile(task_file)
     task = None
     try:
-        task_list = list(xl_file.parse().replace({np.nan:None}).T.to_dict().values())
+        task_list = list(xl_file.parse().replace(
+            {np.nan: None}).T.to_dict().values())
     except Exception as e:
         raise e
     for task in task_list:
@@ -21,18 +27,23 @@ def read_from_task(task_file = 'task.xlsx'):
             task['target_table'] = task['source_table']
     return task_list
 
+
 def get_table_schema(table):
     from google.cloud import bigquery
-    client =  bigquery.Client()
-    dataset_ref = client.dataset(table['source_dataset'], project=table['source_project'])
+    client = bigquery.Client()
+    dataset_ref = client.dataset(
+        table['source_dataset'], project=table['source_project'])
     table_ref = dataset_ref.table(table['source_table'])
-    table = client.get_table(table_ref)  
+
+    table = client.get_table(table_ref)
     schema_field_list = table.schema
 
     # Convert SchemaField type to TableSchema, which beam.io.WriteToBigQuery used to start jobs.
     table_schema = TableSchema()
-    table_schema.fields = [schemafield_to_tableschema(schema_field) for schema_field in schema_field_list]
+    table_schema.fields = [schemafield_to_tableschema(
+        schema_field) for schema_field in schema_field_list]
     return table_schema
+
 
 def schemafield_to_tableschema(schema_field):
     """
@@ -48,29 +59,45 @@ def schemafield_to_tableschema(schema_field):
     return table_field_schema
 
 
-def run():
-    task_list = read_from_task()
-    for task in task_list:
-        migrate_table(task)
+def run(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--task', dest='task', default='task.xls')
+    known_args, pipeline_args_command_line = parser.parse_known_args(argv)
+    pipeline_args = [
+        '--runner=DataflowRunner',
+        '--project=zheng-356906',
+        '--region=asia-east2',
+        '--staging_location=gs://jiawei_bucket/staging',
+        '--temp_location=gs://jiawei_bucket/tmp',
+        #'--job_name=beam_zj_migrate_bigquery_tablea_job',
+        # '--sdk_location=',
+        '--service_account_email=airflow-operator-423@zheng-356906.iam.gserviceaccount.com'
+    ]
+    pipeline_args += pipeline_args_command_line
+    # We use the save_main_session option because one or more DoFn's in this
+    # workflow rely on global context (e.g., a module imported at module level).
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+    task_list = read_from_task_file(known_args.task)
+
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        for task in task_list:
+            print(task, "started!!!!!!")
+            executor.submit(migrate_job, task, pipeline_options)
+            #print(future.result())
+    print("all job finished!!!!!!!!!!")        
 
 
-def migrate_table(task):
+def migrate_job(task, pipeline_options):
     """Run the workflow."""
     table_schema = get_table_schema(task)
 
-    input = task['source_project'] + ':' + task['source_dataset'] + '.' + task['source_table']
-    output = task['target_project'] + ':' + task['target_dataset']+ '.' + task['target_table']
+    input = task['source_project'] + ':' + \
+        task['source_dataset'] + '.' + task['source_table']
+    output = task['target_project'] + ':' + \
+        task['target_dataset'] + '.' + task['target_table']
 
-    pipeline_args = []
-    pipeline_args.extend([
-        '--runner=DataflowRunner',
-        '--project=zheng-356906',
-        #'--region=asia-east2',
-        '--staging_location=gs://jiawei_bucket/staging',
-        '--temp_location=gs://jiawei_bucket/tmp',
-    ])
-
-    with beam.Pipeline(argv=pipeline_args) as p:
+    with beam.Pipeline(options=pipeline_options) as p:
 
         table_records = p | 'ReadTable' >> beam.io.ReadFromBigQuery(
             table=input)
@@ -81,9 +108,11 @@ def migrate_table(task):
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
             write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE
         )
+        p.run()
+    return f"done with task{task}"
 
 
 if __name__ == '__main__':
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\\zj\\coding\\GCP\\zheng-356906-9b7969f2ccbb.json"
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\\zj\\coding\\GCP\service_account\\airflow-operator\\zheng-356906-1c4ce543fca0.json"
     logging.getLogger().setLevel(logging.INFO)
     run()
